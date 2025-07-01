9f044d211d1206ec251a3c23a2011ba85baa0420
hdfaouz <hdfaouzia2@gmail.com>
2025-07-01 11:38:28 +0100

    Création de sousCompetence Mapper

--- src/main/java/com/enaa/enaaskills/Mappers/SousCompetenceMap.java
+++ src/main/java/com/enaa/enaaskills/Mappers/SousCompetenceMap.java
@@ -1,4 +1,18 @@
 package com.enaa.enaaskills.Mappers;
 
+import com.enaa.enaaskills.Dto.CompetenceDto;
+import com.enaa.enaaskills.Dto.SousCompetenceDto;
+import com.enaa.enaaskills.Model.Competence;
+import com.enaa.enaaskills.Model.SousCompetence;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
 public interface SousCompetenceMap {
+    SousCompetence toEntity(SousCompetenceDto dto);
+
+    SousCompetenceDto toDTO(SousCompetence sousCompetence);
+
+    List<SousCompetenceDto> toDTOs(List<SousCompetence> sousCompetences);
 }
